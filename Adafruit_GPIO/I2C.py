c6f0be4b6bf4dd182c6e3aeba7896514a27183c0
ademofj <ademo@126.com>
2018-06-18 18:25:01 +0800

    Update I2C.py

--- Adafruit_GPIO/I2C.py
+++ Adafruit_GPIO/I2C.py
@@ -133,6 +133,7 @@ class Device(object):
     def readList(self, register, length):
         """Read a length number of bytes from the specified register.  Results
         will be returned as a bytearray."""
+        print('*******')
         results = self._bus.read_i2c_block_data(self._address, register, length)
         print('read=%x,%x,%x'%(self._address, register, length))
         self._logger.debug("Read the following from register 0x%02X: %s",
